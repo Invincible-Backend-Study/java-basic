f4c3e25743f40c6e7d7677f8e41c20891ec647c1
Basak <chgy2131@naver.com>
2023-03-29 20:00:48 +0900

    add: 테스트를 위한 프로젝트 추가

---
+++ hyeonsik/java-project-for-test/untitled/src/test/java/C01/Test.java
@@ -0,0 +1,10 @@
+package C01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void testMethod() {
+        assertEquals("abc", "abc");
+    }
+}
